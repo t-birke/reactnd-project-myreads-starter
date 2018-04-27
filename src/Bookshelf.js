442ed7057d9199258b00bd0fdc54ef06e9439f01
Thomas Birke <tbirke@salesforce.com>
2018-04-27 22:14:25 +0200

    split code into components

---
+++ src/Bookshelf.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import Book from './Book'
+import { Link } from 'react-router-dom'
+
+class Bookshelf extends Component {
+  static propTypes = {
+    books: PropTypes.array.isRequired,
+    title: PropTypes.string.isRequired
+  }
+
+  render() {
+    const { books, title, onUpdateShelf } = this.props
+    console.log('bookshelf books',books)
+    return (
+      <div className="bookshelf">
+        <h2 className="bookshelf-title">{title}</h2>
+        <div className="bookshelf-books">
+          <ol className="books-grid">
+            {books.map((book) => (
+              <Book
+                key={book.id}
+                book={book}
+                onUpdateShelf={onUpdateShelf}
+              />)
+            )}
+          </ol>
+        </div>
+      </div>
+    )
+
+  }
+
+}
+
+export default Bookshelf
